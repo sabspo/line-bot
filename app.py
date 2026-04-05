663c15f33d9694c0a74ed432d17afdfe499f0705
sabspo <shuhei32@gmail.com>
2026-04-05 14:55:31 +0900

    fix port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,5 @@ def callback():
     return "OK"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
